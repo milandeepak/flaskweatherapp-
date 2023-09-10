481aa430edd34b9e1898764b495a05ca737d2d82
Milan Deepak <milandeepak06@gmail.com>
2023-09-10 20:07:44 +0530

    add flask app

---
+++ app.py
@@ -0,0 +1,43 @@
+import requests
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+cities = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city_name = request.form.get('city')
+
+        if city_name:
+            url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=f8b794366a2605ac9b420df813e61e25'
+            
+            r = requests.get(url.format(city_name)).json()
+
+            weather_data = {
+                'city': city_name,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'],
+            }
+
+            # Add the weather data to the list of cities
+            cities.append(weather_data)
+
+    # Limit the number of cities per row to 5
+    cities_per_row = [cities[i:i + 5] for i in range(0, len(cities), 5)]
+
+    return render_template('index.html', cities_per_row=cities_per_row)
+
+@app.route('/remove/<int:index>')
+def remove_city(index):
+    # Remove a city from the list based on its index
+    if 0 <= index < len(cities):
+        cities.pop(index)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
